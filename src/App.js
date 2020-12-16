56dc80ef5dc6c12a264b2d70a72568964b46eda2
mtoutside <more.humor.or@gmail.com>
2020-12-16 09:51:05 +0900

    wip favorite list

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import MovieList from "./components/MovieList";
+import MovieListHeading from "./components/MovieListHeading";
+import SearchBox from "./components/SearchBox";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
 
-  const getMovieRequest = async () => {
-    const url = `http://www.omdbapi.com/?s=police academy&apikey=2ff7ac08`;
+  const getMovieRequest = async (searchValue) => {
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=2ff7ac08`;
 
     const response = await fetch(url);
     const responseJson = await response.json();
@@ -18,11 +21,15 @@ const App = () => {
   };
 
   useEffect(() => {
-    getMovieRequest();
-  }, []);
+    getMovieRequest(searchValue);
+  }, [searchValue]);
 
   return (
     <div className="container-fluid movie-app">
+      <div className="row -flex align-items-center mt-4 mb-4">
+        <MovieListHeading heading='Movies' />
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+      </div>
       <div className="row">
         <MovieList movies={movies} />
       </div>
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -4,8 +4,14 @@ const MovieList = (props) => {
   return (
     <>
       {props.movies.map((movie, index) => (
-        <div key={index} className="image-container d-flex justify-content-start m-3">
+        <div
+          key={index}
+          className="image-container d-flex justify-content-start m-3"
+        >
           <img src={movie.Poster} alt="movie" />
+          <div className="overlay d-flex align-items-center justify-content-center">
+            Add to Favourites
+          </div>
         </div>
       ))}
     </>
